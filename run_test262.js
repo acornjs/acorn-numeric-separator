91ac71289ead0b2b8f6aaf71bb268b30f1db7a1c
Adrian Heine <mail@adrianheine.de>
2020-05-03 14:19:49 +0200

    Bump dev dependencies

--- run_test262.js
+++ run_test262.js
@@ -6,20 +6,16 @@ const acorn = require("acorn")
 const Parser = acorn.Parser.extend(require("."))
 
 const unsupportedFeatures = [
-  "BigInt",
   "class-fields-private",
   "class-fields-public",
   "class-methods-private",
   "class-static-fields-private",
   "class-static-fields-public",
   "class-static-methods-private",
-  "dynamic-import",
-  "export-star-as-namespace-from-module",
-  "import.meta"
 ]
 
 run(
-  (content, options) => Parser.parse(content, {sourceType: options.sourceType, ecmaVersion: 9}),
+  (content, options) => Parser.parse(content, {sourceType: options.sourceType, ecmaVersion: 11}),
   {
     testsDirectory: path.dirname(require.resolve("test262/package.json")),
     skip: test => (!test.attrs.features || !test.attrs.features.includes("numeric-separator-literal") || unsupportedFeatures.some(f => test.attrs.features.includes(f))),
